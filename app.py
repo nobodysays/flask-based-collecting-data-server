7bdc1c7abc208c945607c2c986e7f0d593aa7b85
xybyn <nikishtm@gmail.com>
2021-02-03 14:56:45 +0700

    добавлен force upload

--- app.py
+++ app.py
@@ -85,69 +85,107 @@ def upload_year():
     data = json.loads(request.files['json_data'].read())
     return add_to_bd(data)
 
+@app.route('/api/year/upload/force', methods=['POST'])
+def upload_year_force():
+    data = json.loads(request.files['json_data'].read())
+    return add_to_bd_force(data)
+
 @app.route('/api/year/new', methods=['POST'])
 def new_year():
     data = request.get_json()
     return add_to_bd(data)
 
-#TODO: оптимизировать
-def add_to_bd(data):
-    is_year_exists = session.query(exists().where(Year.year == data['year'])).scalar()
+@app.route('/api/year/new/force', methods=['POST'])
+def new_year_force():
+    data = request.get_json()
+    return add_to_bd_force(data)
 
-    current_year = None
-
-    if (not is_year_exists):
-        current_year = Year(year=data['year'])
-
-    else:
-        current_year = session.query(Year).filter_by(year=data['year']).one()
-
-    areas_array = []
+def add_to_bd_force(data):
+    current_year = Year(year=data['year'])
+    areas_array = list()
     areas = data['areas']
     for area in areas:
-        is_area_exists = session.query(exists().where(and_(Area.year_id == current_year.id, Area.name == area['name']))).scalar()
-
-        current_area = None
-
-        if (not is_area_exists):
-            current_area = Area(name=area['name'])
-            areas_array.append(current_area)
-        else:
-            current_area = session.query(Area).filter(and_(Area.name==area['name'], Area.year_id == current_year.id)).one()
-
-        institutes_array = []
+        current_area = Area(name=area['name'])
+        areas_array.append(current_area)
+        institutes_array = list()
         institutes = area['institutes']
         print(f"area {current_area.name}")
         for institute in institutes:
-            is_institute_exists = session.query(exists().where(and_(Institute.area_id == current_area.id,Institute.name == institute['name']))).scalar()
 
-            current_institute = None
-            if not is_institute_exists:
-                current_institute = Institute(name=institute['name'])
-                institutes_array.append(current_institute)
-            else:
-                current_institute = session.query(Institute).filter(and_(Institute.name==institute['name'], Institute.area_id == current_area.id)).one()
+            current_institute = Institute(name=institute['name'])
+            institutes_array.append(current_institute)
             print(f"inst {current_institute.name}")
-            indicators_array = []
-            directions_array = []
+            indicators_array = list()
+            directions_array = list()
             indicators = institute['indicators']
             directions = institute['directions']
             for indicator in indicators:
-                is_indicator_exists = session.query(
-                    exists().where(and_(Indicator.institute_id == current_institute.id, Indicator.indicator==indicator['indicator']))).scalar()
+                current_indicator = Indicator(indicator=indicator["indicator"], value=indicator['value'])
+                indicators_array.append(current_indicator)
 
-                current_indicator = None
-                if not is_indicator_exists:
+            for direction in directions:
+                current_direction = Direction(direction=direction["direction"])
+                directions_array.append(current_direction)
+
+            current_institute.indicators += indicators_array
+            current_institute.directions += directions_array
+
+        current_area.institutes += institutes_array
+
+    current_year.areas += areas_array
+    print("commiting to db")
+    session.add(current_year)
+    session.commit()
+
+    return "ok"
+#TODO: оптимизировать
+def add_to_bd(data):
+    current_year = session.query(Year).filter(Year.year==data['year']).all()
+    if len(current_year) == 0:
+        current_year = Year(year=data['year'])
+    else:
+        current_year = current_year[0]
+
+    areas_array = list()
+    areas = data['areas']
+    for area in areas:
+
+        current_area = session.query(Area).filter(and_(Area.name==area['name'], Area.year_id == current_year.id)).all()
+
+        if len(current_area) == 0:
+            current_area = Area(name=area['name'])
+            areas_array.append(current_area)
+        else:
+            current_area = current_area[0]
+
+        institutes_array = list()
+        institutes = area['institutes']
+        print(f"area {current_area.name}")
+        for institute in institutes:
+
+            current_institute = session.query(Institute).filter(and_(Institute.name==institute['name'], Institute.area_id == current_area.id)).all()
+            if len(current_institute) ==0:
+                current_institute = Institute(name=institute['name'])
+                institutes_array.append(current_institute)
+            else:
+                current_institute = current_institute[0]
+
+            print(f"inst {current_institute.name}")
+            indicators_array = list()
+            directions_array = list()
+            indicators = institute['indicators']
+            directions = institute['directions']
+            for indicator in indicators:
+                current_indicator = session.query(Indicator).filter(and_(Indicator.institute_id == current_institute.id, Indicator.indicator==indicator['indicator'])).all()
+
+                if len(current_indicator) == 0:
                     current_indicator = Indicator(indicator=indicator["indicator"], value=indicator['value'])
                     indicators_array.append(current_indicator)
 
 
             for direction in directions:
-                is_direction_exists = session.query(
-                    exists().where(and_(Direction.institute_id == current_institute.id, Direction.direction == direction['direction']))).scalar()
-
-                current_direction = None
-                if not is_direction_exists:
+                current_direction = session.query(Direction).filter(and_(Direction.institute_id == current_institute.id, Direction.direction == direction['direction'])).all()
+                if len(current_direction) == 0:
                     current_direction = Direction(direction=direction["direction"])
                     directions_array.append(current_direction)
 
